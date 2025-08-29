20141f4f63aecd612720bfb466aff7ec3baebb8f
Victor <victor@mail.com>
2025-08-29 18:27:21 +0200

    Configure iso template

--- iso-standard/configuration.nix
+++ iso-standard/configuration.nix
@@ -1,33 +1,54 @@
+{ config, pkgs, ... }:
+
 {
-  modulesPath,
-  lib,
-  pkgs,
-  ...
-} @ args:
-{
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-    (modulesPath + "/profiles/qemu-guest.nix")
-    ./disk-config.nix
-  ];
-  boot.loader.grub = {
-    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
-    efiSupport = true;
-    efiInstallAsRemovable = true;
+  # Définir stateVersion pour éviter l'avertissement
+  system.stateVersion = "25.05"; # Utilisez la version appropriée
+
+  # Configuration de l'utilisateur nixos avec tous les attributs requis
+  users.users.nixos = {
+    isNormalUser = true;
+    group = "nixos";
+    extraGroups = [ "wheel" ]; # Pour les privilèges sudo
+    # Create password with openssl passwd -6 yourpassword
+    hashedPassword = "$6$2u4jbxPZh.SjV5z5$9axyOyNKHNVRRJHdrSrEQzlxZiV.zbmUWRknMGfm9sGFJ6CnmzkmQd7xXzWFPXoAvnFLEox1Xe39pzl08xuj70";
   };
-  services.openssh.enable = true;
 
-  environment.systemPackages = map lib.lowPrio [
-    pkgs.curl
-    pkgs.gitMinimal
+  # Créer le groupe nixos
+  users.groups.nixos = {};
+
+  # Configuration réseau
+  networking.hostName = "nixos-live";
+  networking.networkmanager.enable = true;
+
+  # Activer SSH
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "no";
+    passwordAuthentication = true;
+  };
+
+  # Autoriser les utilisateurs wheel à utiliser sudo
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = true;
+  };
+
+  # Inclure les outils nécessaires
+  environment.systemPackages = with pkgs; [
+    wget
+    curl
+    git
+    vim
   ];
 
-  users.users.root.openssh.authorizedKeys.keys =
-  [
-    # change this to your ssh key
-    "# CHANGE"
-  ] ++ (args.extraPublicKeys or []); # this is used for unit-testing this module and can be removed if not needed
-
-  system.stateVersion = "25.05";
+  # Configuration spécifique à l'environnement live
+  isoImage = {
+    isoName = "nixos-custom.iso";
+    contents = [
+      {
+        source = "${pkgs.nixos-artwork.wallpapers.simple-dark-gray}/share/artwork/nixos";
+        target = "/background.png";
+      }
+    ];
+  };
 }
