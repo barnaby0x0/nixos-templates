8270ae67f761f9a70d514015b47e4a217159e90e
Victor <victor@mail.com>
2025-09-04 10:02:29 +0200

    Add k8 bootstrap for testing (to be refactored)

---
+++ bootstrap-k8/configuration.nix
@@ -0,0 +1,61 @@
+{
+  modulesPath,
+  lib,
+  pkgs,
+  ...
+} @ args:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ./disk-config.nix
+  ];
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+  services.openssh.enable = true;
+  services.qemuGuest.enable = true;
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
+
+  users.groups.user = {
+      name = "user";
+      members = [ "user" ];
+    };
+
+  users.users.user = {
+      createHome = true;
+      description = "Main User";
+      extraGroups     = [ "networkmanager" "users" "wheel" ];
+      group = "user";
+      home = "/home/user";
+      hashedPassword = "$6$FdYq/J2XbivlPLEG$Jx7ppF65H0vj/.6/118K/d1LUFa4dyucTSk9KTSxlDhIHGH/puUDIIGNx5fcn5H7XgTHPPa.TyyaIugiLw6GF/";
+      isNormalUser = true;
+      shell = pkgs.bash;
+      packages = with pkgs; [
+        home-manager
+      ];
+    };
+
+  users.users.user.openssh.authorizedKeys.keys = ["# CHANGE"] ++ (args.extraPublicKeys or []);
+  users.users.root.openssh.authorizedKeys.keys = ["# CHANGE"] ++ (args.extraPublicKeys or []);
+
+  security.sudo.extraRules = [
+  {
+    users = ["user"];
+    commands = [
+    {
+      command = "ALL";
+      options = ["NOPASSWD"];
+    }
+    ];
+  }
+];
+
+  system.stateVersion = "25.05";
+}
--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
         path = ./bootstrap-vm;
         description = "Bootstrap a NixOS server";
       };
+      bootstrap-k8 = {
+        path = ./bootstrap-k8;
+        description = "Bootstrap a NixOS server";
+      };
       iso-standard = {
         path = ./iso-standard;
         description = "Bootstrap a NixOS server";
